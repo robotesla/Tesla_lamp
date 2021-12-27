c646cff56d97d83869163e5a60e1a963ed02062a
Евгений Александрович <volkov@copter.space>
2021-12-27 03:42:00 +0300

    fix snow, add rain fx

--- teslaLampEsp/teslaLampEsp.ino
+++ teslaLampEsp/teslaLampEsp.ino
@@ -31,42 +31,92 @@ const char *wifi_network_password = "87654321";
 const char *soft_ap_ssid = "Tesla_lamp";
 const char *soft_ap_password = "12345678";
 
-void setup()
+struct FLAKE
 {
+  byte x0 = 0;
+  byte y0 = 30;
+  byte x1 = 0;
+  byte y1 = 29;
+};
 
-  Serial.begin(115200);
-  Serial.setTimeout(3);
-  delay(10);
-  pixels.begin();
-  pixels.setBrightness(127);
-  if (!SPIFFS.begin())
+struct EDROP
+{
+  byte x = 0;
+  byte y = 30;
+};
+
+String getStringValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length() - 1;
+
+  for (int i = 0; i <= maxIndex && found <= index; i++)
   {
-    Serial.println("SPIFFS Mount Failed");
+    if (data.charAt(i) == separator || i == maxIndex)
+    {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+int getXY(uint8_t x, uint8_t y)
+{
+  int num = 0;
+  if (x % 2)
+  {
+    num = ((x + 1) * 30) - y - 1;
+  }
+  else
+  {
+    num = (x * 30) + y;
+  } //num = (x*30)+y;
+  //((x+1)*60)-y;}
+
+  return num;
+}
+
+void listDir(fs::FS &fs, const char *dirname, uint8_t levels)
+{
+  Serial.printf("Listing directory: %s\r\n", dirname);
+
+  File root = fs.open(dirname);
+  if (!root)
+  {
+    Serial.println("- failed to open directory");
     return;
   }
-  //  // We start by connecting to a WiFi network
-  //  WiFi.mode(WIFI_MODE_APSTA);
-  //  WiFi.softAP(soft_ap_ssid, soft_ap_password);
-  //  WiFi.begin(wifi_network_ssid, wifi_network_password);
-  //
-  //  Serial.println();
-  //  Serial.println();
-  //  Serial.print("Waiting for WiFi... ");
-  //
-  //  while (WiFi.status() != WL_CONNECTED) {
-  //    Serial.print(".");
-  //    delay(500);
-  //  }
-  //
-  //  Serial.println("");
-  //  Serial.println("WiFi connected");
-  //  Serial.println("IP address in STA: ");
-  //  Serial.println(WiFi.localIP());
-  //  Serial.print("ESP32 IP as soft AP: ");
-  //  Serial.println(WiFi.softAPIP());
-  //  timeClient.begin();
-  //  server.begin();
-  //  delay(500);
+  if (!root.isDirectory())
+  {
+    Serial.println(" - not a directory");
+    return;
+  }
+
+  File file = root.openNextFile();
+  while (file)
+  {
+    if (file.isDirectory())
+    {
+      Serial.print("  DIR : ");
+      Serial.println(file.name());
+      if (levels)
+      {
+        listDir(fs, file.name(), levels - 1);
+      }
+    }
+    else
+    {
+      Serial.print("  FILE: ");
+      Serial.print(file.name());
+      Serial.print("\tSIZE: ");
+      Serial.println(file.size());
+    }
+    file = root.openNextFile();
+  }
 }
 
 void read_file(uint16_t (*array)[4], fs::FS &fs, const char *path)
@@ -85,7 +135,7 @@ void read_file(uint16_t (*array)[4], fs::FS &fs, const char *path)
     // Serial.println(incomingString);
     for (int i = 0; i < 5; i++)
     {
-      String sub = getValue(incomingString, ',', i);
+      String sub = getStringValue(incomingString, ',', i);
       LED[i] = sub.toInt();
     }
     //pixels.setPixelColor(LED[0], pixels.Color(LED[1], LED[2], LED[3], LED[4]));
@@ -96,25 +146,6 @@ void read_file(uint16_t (*array)[4], fs::FS &fs, const char *path)
   }
   file.close();
   // pixels.show();
-}
-
-String getValue(String data, char separator, int index)
-{
-  int found = 0;
-  int strIndex[] = {0, -1};
-  int maxIndex = data.length() - 1;
-
-  for (int i = 0; i <= maxIndex && found <= index; i++)
-  {
-    if (data.charAt(i) == separator || i == maxIndex)
-    {
-      found++;
-      strIndex[0] = strIndex[1] + 1;
-      strIndex[1] = (i == maxIndex) ? i + 1 : i;
-    }
-  }
-
-  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
 void show_image(uint16_t (*array)[4])
@@ -144,82 +175,167 @@ void change_image(uint16_t (*f_img)[4], uint16_t (*s_img)[4], int time = 3000)
   }
 }
 
-void play_snow(int speed = 1, int time = 1000)
+void play_snow(float speed = 0.5, int time = 10000, byte f_count = 15)
 {
-  int x[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  for (int i = 29; i >= 0; i--)
+  unsigned long start_time = millis();
+
+  // pixels.fill(pixels.Color(0, 0, 0, 3), 0, 299);
+  uint32_t color = pixels.Color(0, 0, 0, 3);
+
+  FLAKE flakes[f_count];
+  for (int i = 0; i < f_count; i++)
   {
-    pixels.fill(pixels.Color(0, 0, 0, 3), 0, 299);
-    for (int k = 0; k < 10; k++)
+    flakes[i].x0 = random(0, 10);
+    flakes[i].x1 = constrain(flakes[i].x0 + random(-1, 2), 0, 9);
+    flakes[i].y0 = 29 - i;
+    flakes[i].y1 = flakes[i].y0 - 1;
+  }
+  while ((millis() - start_time) < time)
+  {
+    pixels.clear();
+    pixels.fill(pixels.Color(0, 0, 3, 0), 0, 299);
+    for (int j = 0; j < 20; j++)
     {
-      x[k] += random(-1, 2);
-      x[k] = constrain(x[k], 0, 10);
-      pixels.setPixelColor(getXY(x[k], (i+(k*3))%30), 0, 0, 20, 100);
+      // pixels.fill(pixels.Color(0, 0, 0, 3), 0, 299);
+      for (int i = 0; i < f_count; i++)
+      {
+        pixels.setPixelColor(getXY(flakes[i].x0, flakes[i].y0), 0, 0, 0, 20 - j);
+        pixels.setPixelColor(getXY(flakes[i].x1, flakes[i].y1), 0, 0, 0, j);
+      }
+      delayMicroseconds(speed * 100.0);
+      pixels.show();
     }
-    pixels.show();
-    delay(200);
+    for (int i = 0; i < f_count; i++)
+    {
+      flakes[i].x0 = flakes[i].x1;
+      flakes[i].x1 = constrain(flakes[i].x0 + random(-1, 2), 0, 9);
+      if (flakes[i].y1 == 0)
+        flakes[i].y1 = 29;
+      flakes[i].y0 = flakes[i].y1;
+      flakes[i].y1 = flakes[i].y0 - 1;
+    }
   }
 }
 
-int getXY(uint8_t x, uint8_t y)
+void play_rain(float speed = 20, int time = 10000, byte f_count = 13)
 {
-  int num = 0;
-  if (x % 2)
-  {
-    num = ((x + 1) * 30) - y - 1;
-  }
-  else
-  {
-    num = (x * 30) + y;
-  } //num = (x*30)+y;
-  //((x+1)*60)-y;}
+  EDROP dp[f_count];
 
-  return num;
+  unsigned long start_time = millis();
+
+  for (int i = 0; i < f_count; i++)
+  {
+    dp[i].x = random(0, 10);
+    dp[i].y = 26 - i*(26/f_count);
+  }
+
+  while ((millis() - start_time) < time)
+  {
+    pixels.clear();
+    pixels.fill(pixels.Color(0, 0, 1, 0), 0, 299);
+
+    for (int i = 0; i < f_count; i++)
+    {
+      pixels.setPixelColor(getXY(dp[i].x, dp[i].y), 1, 1, 5, 0);
+      pixels.setPixelColor(getXY(dp[i].x, dp[i].y+1), 0, 0, 2, 0);
+      pixels.setPixelColor(getXY(dp[i].x, dp[i].y+2), 0, 0, 1, 0);
+    }
+    delayMicroseconds(speed * 1000.0);
+    pixels.show();
+
+    for (int i = 0; i < f_count; i++)
+    {
+      dp[i].y = dp[i].y - 1;
+      if (dp[i].y == 0)
+        dp[i].y = 26;
+    }
+  }
+}
+
+void setup()
+{
+
+  Serial.begin(115200);
+  Serial.setTimeout(3);
+  delay(1000);
+  pixels.begin();
+  pixels.setBrightness(127);
+  if (!SPIFFS.begin())
+  {
+    Serial.println("SPIFFS Mount Failed");
+    return;
+  }
+
+  listDir(SPIFFS, "/", 0);
+  //  // We start by connecting to a WiFi network
+  //  WiFi.mode(WIFI_MODE_APSTA);
+  //  WiFi.softAP(soft_ap_ssid, soft_ap_password);
+  //  WiFi.begin(wifi_network_ssid, wifi_network_password);
+  //
+  //  Serial.println();
+  //  Serial.println();
+  //  Serial.print("Waiting for WiFi... ");
+  //
+  //  while (WiFi.status() != WL_CONNECTED) {
+  //    Serial.print(".");
+  //    delay(500);
+  //  }
+  //
+  //  Serial.println("");
+  //  Serial.println("WiFi connected");
+  //  Serial.println("IP address in STA: ");
+  //  Serial.println(WiFi.localIP());
+  //  Serial.print("ESP32 IP as soft AP: ");
+  //  Serial.println(WiFi.softAPIP());
+  //  timeClient.begin();
+  //  server.begin();
+  //  delay(500);
 }
 
 void loop()
 {
-//   int LED[5] = {0, 255, 0, 0, 0};
-//   while (Serial.available())
-//   {
-//     String incomingString = Serial.readString();
-//     for (int i = 0; i < 5; i++)
-//     {
-//       String sub = getValue(incomingString, ',', i);
-//       LED[i] = sub.toInt();
-//     }
-//     pixels.setPixelColor(LED[0], pixels.Color(LED[1], LED[2], LED[3], LED[4]));
-//     Serial.println(incomingString);
-//  
-//   }
-//   if (LED[0] == 299)
-//   {
-//     //    for (int k = 0; k < 300; k++)
-//     //    {
-//     //      Serial.println( pixels.getPixelColor(k));
-//     //    }
-//     pixels.show();
-//     delay(10);
-//   }
+  //   int LED[5] = {0, 255, 0, 0, 0};
+  //   while (Serial.available())
+  //   {
+  //     String incomingString = Serial.readString();
+  //     for (int i = 0; i < 5; i++)
+  //     {
+  //       String sub = getValue(incomingString, ',', i);
+  //       LED[i] = sub.toInt();
+  //     }
+  //     pixels.setPixelColor(LED[0], pixels.Color(LED[1], LED[2], LED[3], LED[4]));
+  //     Serial.println(incomingString);
+  //
+  //   }
+  //   if (LED[0] == 299)
+  //   {
+  //     //    for (int k = 0; k < 300; k++)
+  //     //    {
+  //     //      Serial.println( pixels.getPixelColor(k));
+  //     //    }
+  //     pixels.show();
+  //     delay(10);
+  //   }
   // pixels.clear();
   // pixels.show();
 
-  uint16_t one_pic[300][4];
-  uint16_t two_pic[300][4];
-  for (int j = 1; j < 8; j++) //просмотр всех изображений
-  {
-    String filename_one = String("/afric")+String(j)+String(".txt");
-    String filename_two = String("/afric")+String(j+1)+String(".txt");
-    read_file(one_pic, SPIFFS, filename_one.c_str());
-    read_file(two_pic, SPIFFS, filename_two.c_str());
+  // uint16_t one_pic[300][4];
+  // uint16_t two_pic[300][4];
+  // for (int j = 1; j < 8; j++) //просмотр всех изображений
+  // {
+  //   String filename_one = String("/winter/")+String(j)+String(".txt");
+  //   String filename_two = String("/winter/")+String(j+1)+String(".txt");
+  //   read_file(one_pic, SPIFFS, filename_one.c_str());
+  //   read_file(two_pic, SPIFFS, filename_two.c_str());
 
-    change_image(one_pic,two_pic,3000);
-  }
+  //   change_image(one_pic,two_pic,3000);
+  // }
 
   // read_file(one_pic, SPIFFS, "/8.txt");
   // read_file(two_pic, SPIFFS, "/1.txt");
 
   // change_image(one_pic,two_pic,1000);
 
-  // play_snow();
+  play_snow();
+  play_rain();
 }
